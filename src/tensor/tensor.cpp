5fc0cdee8a9a2bacd7b54ed3c62508ae552d9cc7
SharpDrJ <543538339@qq.com>
2026-01-16 16:28:41 +0800

    完成作业1

--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -164,27 +164,114 @@ void Tensor::debug() const {
 }
 
 bool Tensor::isContiguous() const {
-    TO_BE_IMPLEMENTED();
+    const auto &shape = this->shape();
+    const auto &strides = this->strides();
+
+    // C++ 中专门用来表示两个指针之间距离的类型
+    ptrdiff_t expected_stride = 1;
+
+    // 从最后一维开始向前检查，shape.size()表示张量的维度数量
+    for (int i = (int)shape.size() - 1; i >= 0; i--) {
+        // 如果该维度的大小为 0，说明张量为空，它是连续的
+        if (shape[i] == 0) {
+            return true;
+        }
+
+        if (shape[i] > 1 && strides[i] != expected_stride) {
+            return false;
+        }
+
+        expected_stride *= shape[i];
+    }
+
     return true;
 }
 
 tensor_t Tensor::permute(const std::vector<size_t> &order) const {
-    TO_BE_IMPLEMENTED();
-    return std::shared_ptr<Tensor>(new Tensor(_meta, _storage));
+    size_t ndim_ = this->ndim();
+
+    if (order.size() != ndim_) {
+        EXCEPTION_SHAPE_MISMATCH;
+    }
+
+    std::vector<size_t> new_shape(ndim_);
+    std::vector<ptrdiff_t> new_strides(ndim_);
+
+    for (size_t i = 0; i < ndim_; ++i) {
+        size_t old_dim = order[i];
+
+        if (old_dim >= ndim_) {
+            EXCEPTION_SHAPE_MISMATCH;
+        }
+
+        new_shape[i] = _meta.shape[old_dim];
+        new_strides[i] = _meta.strides[old_dim];
+    }
+
+    TensorMeta new_meta{_meta.dtype, std::move(new_shape), std::move(new_strides)};
+
+    return std::shared_ptr<Tensor>(new Tensor(new_meta, _storage, _offset));
 }
 
 tensor_t Tensor::view(const std::vector<size_t> &shape) const {
-    TO_BE_IMPLEMENTED();
-    return std::shared_ptr<Tensor>(new Tensor(_meta, _storage));
+    size_t new_numel = 1;
+    for (auto s : shape) {
+        new_numel *= s;
+    }
+    if (new_numel != this->numel()) {
+        EXCEPTION_SHAPE_MISMATCH;
+    }
+
+    if (!this->isContiguous()) {
+        EXCEPTION_SHAPE_MISMATCH;
+    }
+
+    std::vector<ptrdiff_t> new_strides(shape.size());
+    ptrdiff_t stride = 1;
+    for (int i = (int)shape.size() - 1; i >= 0; i--) {
+        new_strides[i] = stride;
+        stride *= shape[i];
+    }
+
+    TensorMeta new_meta{_meta.dtype, shape, new_strides};
+
+    return std::shared_ptr<Tensor>(new Tensor(new_meta, _storage, this->_offset));
 }
 
 tensor_t Tensor::slice(size_t dim, size_t start, size_t end) const {
-    TO_BE_IMPLEMENTED();
-    return std::shared_ptr<Tensor>(new Tensor(_meta, _storage));
+    size_t ndim_ = this->ndim();
+
+    if (dim >= ndim_) {
+        EXCEPTION_SHAPE_MISMATCH;
+    }
+    if (start >= end || end > _meta.shape[dim]) {
+        EXCEPTION_SHAPE_MISMATCH;
+    }
+
+    std::vector<size_t> new_shape = this->shape();
+    new_shape[dim] = end - start;
+
+    std::vector<ptrdiff_t> new_strides = this->strides();
+
+    size_t offset_delta = start * this->strides()[dim] * this->elementSize();
+    size_t new_offset = this->_offset + offset_delta;
+
+    TensorMeta new_meta{this->dtype(), std::move(new_shape), std::move(new_strides)};
+    return std::shared_ptr<Tensor>(new Tensor(new_meta, _storage, new_offset));
 }
 
 void Tensor::load(const void *src_) {
-    TO_BE_IMPLEMENTED();
+    // this->类似于python的self，其实可以不写
+    core::context().setDevice(this->deviceType(), this->deviceId());
+
+    size_t total_bytes = this->numel() * this->elementSize();
+
+    core::context().runtime().api()->memcpy_sync(
+        this->data(),      // 目标地址，张量的内存指针
+        src_,              // 源地址，传入的外部数据指针
+        total_bytes,       // 复制的字节数
+        LLAISYS_MEMCPY_H2D // 复制方向：从主机到设备
+    );
 }
 
 tensor_t Tensor::contiguous() const {
